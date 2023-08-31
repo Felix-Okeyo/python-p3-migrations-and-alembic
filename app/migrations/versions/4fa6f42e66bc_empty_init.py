473aa76e3407df185d183b69818cce676ff3fd0d
Felix-Okeyo <felixokeyo20@gmail.com>
2023-08-31 16:46:51 +0300

    done

---
+++ app/migrations/versions/4fa6f42e66bc_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 4fa6f42e66bc
+Revises: 
+Create Date: 2023-08-31 16:28:44.379063
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '4fa6f42e66bc'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
